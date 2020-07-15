ebb63f16d1582209dc7d3dc59a541393cd13100c
Kelminator <kemuel.torres1@upr.edu>
2020-07-15 18:34:53 -0500

    ADDED Controller Functionality.

--- Tetris/src/application/Tetris.java
+++ Tetris/src/application/Tetris.java
@@ -1,5 +1,10 @@
 package application;
 
 public class Tetris{
+    public static final int MOVE;
+    public static final int SIZE;
+    public static int XMAX;
+    public static int YMAX;
+    public static int [][] GRID;
 
 }
